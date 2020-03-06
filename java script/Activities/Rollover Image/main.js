6d29f0e67a71bb24a54b21c022c866da795f9c92
Mr.C.Ebenesh <60320143+EbeneshMarticlues@users.noreply.github.com>
2020-03-06 16:18:51 +0530

    Update main.js

--- java script/Activities/Rollover Image/main.js
+++ java script/Activities/Rollover Image/main.js
@@ -1,11 +1,11 @@
 function bigImg(){
-    let x=["img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg","img6.jpg"]
+    let x=["img1.png","img2.png","img3.png"]
     let r = x[Math.floor(Math.random() * x.length)];
     document.getElementById("big").src =r;
   }
   
   function normalImg(){
-    let x=["img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg","img6.jpg"]
+    let x=["img1.png","img2.png","img3.png"]
     let r = x[Math.floor(Math.random() * x.length)];
     document.getElementById("big").src =r;
   }
