8cb4d8a88899da842074a521f6dcbcbd222549cd
Mr.C.Ebenesh <60320143+EbeneshMarticlues@users.noreply.github.com>
2020-03-26 11:18:11 +0530

    Create timeout.js

---
+++ java script/Activities/timeout.js
@@ -0,0 +1,90 @@
+var time = document.getElementById("time");
+setTimeout(function(i){
+	
+	
+	time.innerHTML=10
+	setTimeout(function(i){
+	
+	
+	time.innerHTML=9
+	setTimeout(function(i){
+	
+	
+	time.innerHTML=8
+	setTimeout(function(i){
+	
+	
+	time.innerHTML=7
+	setTimeout(function(i){
+	
+	
+	time.innerHTML=6
+	setTimeout(function(i){
+	
+	
+	time.innerHTML=5
+	setTimeout(function(i){
+	
+	
+	time.innerHTML=4
+	setTimeout(function(i){
+	
+	
+	time.innerHTML=3
+	setTimeout(function(i){
+	
+	
+	time.innerHTML=2
+	setTimeout(function(i){
+	
+	
+	time.innerHTML=1
+		setTimeout(function(i){
+	
+	
+	time.innerHTML="Happy Holiday"
+	var i =0;
+	
+	setInterval(function(){
+	    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    document.body.style.background = bgColor;
+	i++;
+	},1000);
+	
+	
+	
+},1000);
+	
+	
+	
+},1000);
+	
+	
+},1000);
+	
+	
+},1000);
+	
+	
+},1000);
+	
+	
+},1000);
+	
+	
+},1000);
+
+	
+	
+},1000);
+	
+},1000);
+	
+},1000);
+	
+},1000);
