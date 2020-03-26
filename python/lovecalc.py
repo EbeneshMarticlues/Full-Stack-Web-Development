ebfc68f04cec3af92a85084329b4a4d48b34e68d
Mr.C.Ebenesh <60320143+EbeneshMarticlues@users.noreply.github.com>
2020-03-26 13:10:27 +0530

    Update lovecalc.py

--- python/lovecalc.py
+++ python/lovecalc.py
@@ -1,38 +1,20 @@
 import random
 
-a=input("Give Your Name : ")
-b=input("Give GF Name : ")
+a=int(random.random()*101)
 
-print(a)
+b=input("Enter Your Name : ")
 
-print(b)
+c=input("Enter Your Lovers Name : ")
 
-a = int(random.random()*101)
-
-if(a<10):
-    print("Your Love :"+str(a)+"%" )
-    print("Your love is Low")
-elif(a>10&a<50):
-    print("Your Love :"+str(a)+"%" )
-    print("Your love is nutral")
-elif():
-    print("Your Love :"+str(a)+"%" )
-    print("Your love is Strong ")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if(a<=50):
+    print("Your Love : "+str(a)+"%")
+    print("Low")
+elif(a>=50&a<=99):
+    print("Your Love : "+str(a)+"%")
+    print("nutral")
+elif(a==100):
+    print("Your Love : "+str(a)+"%")
+    print("Strong")
 
 
 
